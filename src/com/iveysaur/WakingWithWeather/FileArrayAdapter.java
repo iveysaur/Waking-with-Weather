d4cf76c919a56fcdf5a52444c8885b1d55271e7b
ivey <iveysaurrr@gmail.com>
2014-06-08 22:03:28 -0400

    Create custom view of files

--- src/com/iveysaur/WakingWithWeather/FileArrayAdapter.java
+++ src/com/iveysaur/WakingWithWeather/FileArrayAdapter.java
@@ -12,5 +12,32 @@ public class FileArrayAdapter extends ArrayAdapter<Option> {
 		id = textViewResourceId;
 		items = objects;
 	}
+
+	public Option getItem(int i) {
+		return items.get(i);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View v = convertView;
+		if (v == null) {
+			LayoutInflater vi = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			v = vi.inflate(id, null);
+		}
+
+		final Option o = items.get(position);
+
+		if (o != null) {
+			TextView t1 = (Textview) v.findViewById(R.id.TextView01);
+			TextView t2 = (Textview) v.findViewById(R.id.TextView02);
+			if (t1 != null) {
+				t1.setText(o.getName());
+			}
+			if (t2 != null) {
+				t2.setText(o.getName());
+			}
+		}
+		return v;
+	}
 }
 
