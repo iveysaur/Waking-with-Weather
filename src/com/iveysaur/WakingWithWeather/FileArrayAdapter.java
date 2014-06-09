eae38c07fc0f9453806af7869d3efa940156c62f
ivey <iveysaurrr@gmail.com>
2014-06-08 21:44:06 -0400

    Added FileArrayAdapter class

---
+++ src/com/iveysaur/WakingWithWeather/FileArrayAdapter.java
@@ -0,0 +1,16 @@
+package com.iveysaur.WakingWithWeather;
+
+public class FileArrayAdapter extends ArrayAdapter<Option> {
+	private Context c;
+	private int id;
+	private List<Option> items;
+
+	public FileArrayAdapter(Context context, int textViewResourceId,
+			List<Option> objects) {
+		super(context, textViewResourceId, objects);
+		c = context;
+		id = textViewResourceId;
+		items = objects;
+	}
+}
+
--- src/com/iveysaur/WakingWithWeather/MainActivity.java
+++ src/com/iveysaur/WakingWithWeather/MainActivity.java
@@ -3,6 +3,7 @@ package com.iveysaur.WakingWithWeather;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
