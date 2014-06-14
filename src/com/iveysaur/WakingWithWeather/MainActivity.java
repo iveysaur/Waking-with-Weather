e5181783b48af42c84265a123ecce1010ae95f75
ivey <iveysaurrr@gmail.com>
2014-06-14 10:43:54 -0400

    Move fileChooser to own file and revert MainActivity

--- src/com/iveysaur/WakingWithWeather/MainActivity.java
+++ src/com/iveysaur/WakingWithWeather/MainActivity.java
@@ -1,73 +1,15 @@
 package com.iveysaur.WakingWithWeather;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.Toast;
 
-public class MainActivity extends ListActivity
+public class MainActivity extends Activity
 {
-	private File curDir;
-	private FileArrayAdapter adapter;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-		curDir = new File("/sdcard/");
-		files(curDir);
-    }
-
-	private void files(File f) {
-		File[]dirs = f.listFiles();
-
-		this.setTitle("Current Dir: " + f.getName());
-		List<Option>dir = new ArrayList<Option>();
-		List<Option>files = new ArrayList<Option>();
-
-		try {
-			for (File ff: dirs) {
-				if (ff.isDirectory()) {
-					dir.add(new Option(ff.getName(), "Folder", ff.getAbsolutePath()));
-				} else {
-					files.add(new Option(ff.getName(), "File Size: " + ff.length(), ff.getAbsolutePath()));
-				}
-			}
-		} catch(Exception e) {
-		}
-
-		Collections.sort(dir);
-		Collections.sort(files);
-
-		dir.addAll(files);
-
-		if (!f.getName().equalsIgnoreCase("sdcard")) {
-			dir.add(0, new Option("...", "Parent Directory", f.getParent()));
-		}
-		adapter = new FileArrayAdapter(MainActivity.this, R.layout.file_view, dir);
-		this.setListAdapter(adapter);
-	}
-
+	/** Called when the activity is first created. */
 	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
-		Option o = adapter.getItem(position);
-
-		if (o.getData().equalsIgnoreCase("folder") || o.getData().equalsIgnoreCase("parent directory")) {
-			curDir = new File(o.getPath());
-			files(curDir);
-		} else {
-			onFileClick(o);
-		}
-	}
-
-	private void onFileClick(Option o) {
-		Toast.makeText(this, "File Clicked: " + o.getName(), Toast.LENGTH_SHORT).show();
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 	}
 }
